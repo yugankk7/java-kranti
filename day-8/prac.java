34417ff207e1c4c97c4f9385cbfd0e5d9c862cc6
Yugank <52129410+yugankk7@users.noreply.github.com>
2021-07-02 14:21:29 +0530

    Update prac.java

--- day-8/prac.java
+++ day-8/prac.java
@@ -1,1 +1,21 @@
+package prac;
 
+
+public class Prac {
+
+    public static void main(String[] args) {
+        
+        Demo d1 = new Demo();
+        d1.disp();
+        Demo d2 = new Demo();
+        d1.instVar = "Hello from d1";
+        d1.disp();
+        d2.instVar = "hello form d2";
+        d2.disp();
+
+        Demo d3 = new Demo("D3 constructor");
+        d3.disp();
+
+    }
+
+}
