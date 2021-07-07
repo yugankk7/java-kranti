aa1bbb096c8e4ecb31a6cf1f8d5ba1a08eb8c321
Yugank <52129410+yugankk7@users.noreply.github.com>
2021-07-07 22:37:18 +0530

    Update h.java

--- Day-12/h.java
+++ Day-12/h.java
@@ -1,14 +1,13 @@
-package BasicJavaTest;
 
 import java.util.Scanner;
 
 public class h {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows...");
-        int rows = sc.nextInt();
-        for(int i = 1;i <= rows; i++){
-            int num = i;
+        System.out.println("Enter the number of rows...");              //1
+        int rows = sc.nextInt();                                        //2 5  
+        for(int i = 1;i <= rows; i++){                                  //3 6 8
+            int num = i;                                                //4 7 9 10
             for(int j = 1; j <= i; j++){
                 System.out.print(num+" ");
                 num = num + rows - j;
