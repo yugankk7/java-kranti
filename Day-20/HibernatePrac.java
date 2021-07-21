b115e1b764daf0458458893cf5f1f7e7709bb027
Yugank <52129410+yugankk7@users.noreply.github.com>
2021-07-21 17:06:23 +0530

    Create HibernatePrac.java

---
+++ Day-20/HibernatePrac.java
@@ -0,0 +1,25 @@
+// package hibernate_prac;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+class HibernatePrac {
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSl=false";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database" + jdbcUrl);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection succesfull");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
